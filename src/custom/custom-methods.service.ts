8c813c4fa52d3aa8da619be3166668177d88ff59
Innokenty Ivanov <Kenya-West@outlook.com>
2023-04-17 22:41:09 +0600

    Add timer methods

--- src/custom/custom-methods.service.ts
+++ src/custom/custom-methods.service.ts
@@ -5,10 +5,30 @@ import { Logger } from "../utils/logger";
 
 @singleton()
 export class CustomMethodsService {
+    protected interval?: NodeJS.Timer;
+
     constructor() {
         this.init();
     }
     public init() {
+        this.startTimer(500);
+
+        // Any logic to setup your custom script here
+    }
+
+    public startTimer(time: number) {
+        this.interval = setInterval(() => {
+            this.renderButton();
+        }, time);
+    }
+
+    public stopTimer(): void {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+    }
+
+    private renderButton() {
         const controlService = container.resolve<ControlComposeService>(ControlComposeService);
         // TODO: Remove `any`
         controlService.composeAndRender(ControlCollection.exampleButton as any);
